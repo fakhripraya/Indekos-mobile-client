ff41088076d71031a345fbc8176139cac342388e
fakhripraya <fakhri@indomaret.co.id>
2021-01-12 09:18:37 +0700

    Update owner_regist.js

--- client/app/screens/CreateUserScreen/owner_regist.js
+++ client/app/screens/CreateUserScreen/owner_regist.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import { AppStyle } from '../../config/app.config';
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import { SecondBackground } from '../../components/Backgrounds/create_user_background'
 
 export default function Agreement() {
+
+    useEffect(() => {
+        effect
+        return () => {
+            cleanup
+        }
+    }, [])
+
     return (
         <SecondBackground>
+            <View>
+
+            </View>
+            <View>
+                <ScrollView>
+                    <View>
+
+                    </View>
+                </ScrollView>
+            </View>
+            <View>
+
+            </View>
         </SecondBackground>
     )
 }
